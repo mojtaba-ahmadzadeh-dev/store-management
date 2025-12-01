3b554dc3076d1390b112194e9d40a1c0e59aa441
Mojtaba Ahmadzadeh <mojtabaahmadzadeh.2024@gmail.com>
2025-12-01 13:02:53 +0330

    feat(database): configure Sequelize with environment variables

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { notFoundHandler } from './src/exception/not-found.exception.js';
+import { sequelize } from './src/configs/sequelize.config.js';
 import { errorHandler } from './src/exception/error-handler.js';
 import SwaggerConfig from './src/configs/swagger.config.js';
 
@@ -14,6 +15,7 @@ class App {
 
         this.initMiddleware();
         this.initSwagger();
+        this.initDatabase();;
         this.initErrorHandling();
     }
 
@@ -25,6 +27,15 @@ class App {
 
     initSwagger() {
         SwaggerConfig(this.app)
+    }
+
+    async initDatabase() {
+        try {
+            await sequelize.authenticate();
+            console.log('Database connection has been established successfully.');
+        } catch (error) {
+            console.log('Unable to connect to the database:', error);
+        }
     }
 
     // error handler
---
+++ src/configs/sequelize.config.js
@@ -0,0 +1,15 @@
+import { Sequelize } from "sequelize";
+import dotenv from 'dotenv'
+dotenv.config()
+
+const sequelize = new Sequelize({
+    dialect: process.env.DB_DIALECT,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    logging: false
+})
+
+export { sequelize }
