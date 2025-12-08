f40eac9b520c210ba35bcfd8b5927db21dafa3d9
Mojtaba Ahmadzadeh <mojtabaahmadzadeh.2024@gmail.com>
2025-12-08 12:57:58 +0330

    feat: add Payment model and payment status constants

--- src/configs/model.init.js
+++ src/configs/model.init.js
@@ -5,6 +5,7 @@ import { Category } from "../modules/category/category.model.js";
 import { Basket } from "../modules/basket/basket.model.js";
 import { Order, OrderItem } from "../modules/order/order.model.js";
 import { Permission, Role, RolePermissions } from "../modules/RBAC/rbac.model.js";
+import { Payment } from "../modules/payment/payment.model.js";
 
 const initDatabase = async () => {
     User.hasMany(Basket, { foreignKey: 'user_id', onDelete: 'CASCADE' });
@@ -25,6 +26,9 @@ const initDatabase = async () => {
     Order.hasMany(OrderItem, { foreignKey: 'order_id', onDelete: 'CASCADE' });
     OrderItem.belongsTo(Order, { foreignKey: 'order_id' });
 
+    Order.hasMany(Payment, { foreignKey: 'order_id', onDelete: 'CASCADE' });
+    Payment.belongsTo(Order, { foreignKey: 'order_id' });
+
     Role.belongsToMany(Permission, {
         through: RolePermissions,
         foreignKey: 'roleId',
@@ -38,6 +42,9 @@ const initDatabase = async () => {
         otherKey: 'roleId',
         as: 'roles'
     });
+
+    // Payment.sync()
+    // Order.sync()
 
     // await sequelize.sync({ alter: true });
     // console.log("Database initialized with Order & OrderItem!");
---
+++ src/constant/payment_status.constant.js
@@ -0,0 +1,7 @@
+export const PaymentStatus = {
+  PENDING: 'pending',      
+  PAID: 'paid',           
+  FAILED: 'failed',     
+  REFUNDED: 'refunded',   
+  CANCELLED: 'cancelled',  
+};
