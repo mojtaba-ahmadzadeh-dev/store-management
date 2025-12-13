60ba5db2925601c1117e0ef379460b0a8791e2e8
Mojtaba Ahmadzadeh <mojtabaahmadzadeh.2024@gmail.com>
2025-12-13 14:04:48 +0330

    feat(auth): add authGuard to /me and /logout routes

--- src/modules/auth/auth.routes.js
+++ src/modules/auth/auth.routes.js
@@ -3,13 +3,12 @@ import authController from "./auth.controller.js";
 import { validateCheckOTP, validateSendOTP } from "./auth.validation.js";
 import { authGuard } from "../../middlewares/guard/auth.guard.js";
 
-
 const router = Router();
 
 router.post('/send-otp', validateSendOTP, authController.sendOTP);
 router.post('/check-otp', validateCheckOTP, authController.checkOTP);
 router.post('/refresh-token', authController.refreshToken);
-router.get('/me', authController.getMe);
-router.post('/logout', authController.logout);
+router.get('/me', authGuard(), authController.getMe);
+router.post('/logout', authGuard(), authController.logout);
 
 export { router as AuthRoutes };
